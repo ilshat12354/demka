9c06cef7d30c3ac8a3ca27680437a7a597503e29
Ilshat <ilshat12354@gmail.com>
2023-05-29 02:22:57 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using demka.windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            List<tkani> tkaniinfos = new List<tkani>();
+
+            using (demkaEntities db = new demkaEntities())
+            {
+                foreach (tkani tkani in db.tkani)
+                {
+                    tkaniinfos.Add(tkani);
+                } 
+            }
+            Tkani.ItemsSource = tkaniinfos; 
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            LoginWindow lg = new LoginWindow();
+            lg.Show();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ windows/LoginWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace demka.windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+
+            List<string> roleslist = new List<string>();
+
+            using(demkaEntities db = new demkaEntities())
+            {
+                foreach(role role in db.role)
+                {
+                    roleslist.Add(role.role1);
+                }
+            }
+            Role.ItemsSource = roleslist;
+        }
+
+        private void Login_button_Click(object sender, RoutedEventArgs e)
+        {
+            string role = (string)Role.SelectedItem;
+            int id_role = 0;
+
+            using(demkaEntities db = new demkaEntities())
+            {
+                Polzovatel pz = new Polzovatel();
+                role role1 = new role();
+
+                pz.login = Login_textbox.Text;
+                pz.password = Password_textbox.Text;
+                
+                foreach(role r in db.role)
+                {
+                    if(r.role1 == role)
+                    {
+                        id_role = r.id_role;
+                    }
+                }
+                pz.id_role = id_role;
+
+                db.Polzovatel.Add(pz);
+                db.SaveChanges();
+            }
+            MessageBox.Show("success");
+
+            this.Hide();
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+    }
+}
